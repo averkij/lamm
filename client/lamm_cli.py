35330a4f9730b72323739c081e32b52af20f10f1
Sergei Averkiev <averoo@gmail.com>
2023-09-03 19:41:21 +0300

    Add client.

--- client/lamm_cli.py
+++ client/lamm_cli.py
@@ -1,4 +1,45 @@
 import requests
+import os
+import json
 
-with open("./client/test_file.json", "rb") as file:
-    r = requests.post("http://localhost:80/sbs/create", files={"test_file.json": file})
+
+def create_sbs(name, first, second, address="localhost:80"):
+    if not "name" in first or not "name" in second:
+        print("Please, provide model names.")
+        return
+
+    if not "data" in first or not "data" in second:
+        print("Please, provide paths to the files with data.")
+        return
+
+    if not os.path.isfile(first["data"]):
+        print(f"{first['data']} does not exist")
+        return
+
+    if not os.path.isfile(first["data"]):
+        print(f"{second['data']} does not exist")
+        return
+
+    if not name:
+        print("Please, provide SBS name.")
+        return
+
+    with open(first["data"], "rb") as file_1:
+        with open(second["data"], "rb") as file_2:
+            response = requests.post(
+                f"http://{address}/sbs/create",
+                data={
+                    "name": name,
+                    "model_1": first["name"],
+                    "model_2": second["name"],
+                    "filename_1": first["data"],
+                    "filename_2": second["data"],
+                },
+                files={first["data"]: file_1, second["data"]: file_2},
+            )
+
+    # print(r.status_code)
+
+    res = json.loads(response.content.decode("utf-8"))
+
+    return res
