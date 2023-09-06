b35099721bc3aaf4b580c2535d5b305078381b16
Sergei Averkiev <averoo@gmail.com>
2023-09-06 11:46:30 +0300

    Test scripts.

---
+++ backend/test_backend.py
@@ -0,0 +1,29 @@
+# %%
+import db_helper
+
+sbs_guid = "b9a14e6583b942ed9247b9200a4ca9c2"
+user_guid = "new_user_3"
+
+
+db_helper.ensure_user_exists(sbs_guid, user_guid)
+# %%
+
+
+tasks = db_helper.get_tasks(sbs_guid, user_guid, n=3)
+
+# %%
+tasks
+
+# %%
+
+# EVENT_GET = 0
+# EVENT_LEFT_BETTER = 1
+# EVENT_RIGHT_BETTER = 2
+# EVENT_BOTH_GOOD = 3
+# EVENT_BOTH_BAD = 4
+# EVENT_SKIP = 5
+
+
+tasks = db_helper.resolve_task(sbs_guid, user_guid, 3, 1)
+
+# %%
--- client/test.py
+++ client/test.py
@@ -6,7 +6,7 @@ first_model = {"name": "gpt-3.5-turbo", "data": "./test_samples_1.json"}
 second_model = {"name": "giga-chat", "data": "./test_samples_2.json"}
 
 res = lamm_cli.create_sbs(
-    name="Test SBS 1",
+    name="Test SBS 4",
     first=first_model,
     second=second_model,
 )
@@ -14,4 +14,6 @@ res = lamm_cli.create_sbs(
 # %%
 res
 
-# %%
+# f687064dd4e54e679e9db56d97e14e01
+
+# sbs_guid = "f687064dd4e54e679e9db56d97e14e01"
